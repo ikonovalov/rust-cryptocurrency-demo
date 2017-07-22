df862dc0c6ab5c77d9bd598bea79839f7d72098e
ikonovalov <konovalov84@gmail.com>
2017-07-22 22:23:58 +0300

    Initial

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate exonum;
+extern crate router;
+extern crate bodyparser;
+extern crate iron;
+
+use exonum::blockchain::{self, Blockchain, Service, GenesisConfig,
+                         ValidatorKeys, Transaction, ApiContext};
+use exonum::node::{Node, NodeConfig, NodeApiConfig, TransactionSend,
+                   ApiSender, NodeChannel};
+use exonum::messages::{RawTransaction, FromRaw, Message};
+use exonum::storage::{Fork, MemoryDB, MapIndex};
+use exonum::crypto::{PublicKey, Hash};
+use exonum::encoding::{self, Field};
+use exonum::api::{Api, ApiError};
+use iron::prelude::*;
+use iron::Handler;
+use router::Router;
+
+fn main() {
+    println!("Hello, world!");
+}
