72da5590c40fed3ddaea24b4e93c9221ec7d5ca5
ikonovalov <konovalov84@gmail.com>
2017-07-25 00:15:44 +0300

    Fix strange code reordering. Rust spec?

--- src/main.rs
+++ src/main.rs
@@ -34,53 +34,6 @@ const INIT_BALANCE: u64 = 100;
 fn main() {
     exonum::helpers::init_logger().unwrap();
 
-    // Initialize Blockchain
-    let db = MemoryDB::new();
-    let services: Vec<Box<Service>> = vec![];
-    let blockchain = Blockchain::new(Box::new(db), services);
-
-
-    // Create Keys
-    let (consensus_public_key, consensus_secret_key) = exonum::crypto::gen_keypair();
-    let (service_public_key, service_secret_key) = exonum::crypto::gen_keypair();
-
-
-    // Configure Node
-    let validator_keys = ValidatorKeys {
-        consensus_key: consensus_public_key,
-        service_key: service_public_key,
-    };
-    let genesis = GenesisConfig::new(vec![validator_keys].into_iter());
-
-
-    let api_address = "0.0.0.0:8000".parse().unwrap();
-    let api_cfg = NodeApiConfig {
-        public_api_address: Some(api_address),
-        ..Default::default()
-    };
-
-    let peer_address = "0.0.0.0:2000".parse().unwrap();
-
-    // Complete node configuration
-    let node_cfg = NodeConfig {
-        listen_address: peer_address,
-        peers: vec![],
-        service_public_key,
-        service_secret_key,
-        consensus_public_key,
-        consensus_secret_key,
-        genesis,
-        external_address: None,
-        network: Default::default(),
-        whitelist: Default::default(),
-        api: api_cfg,
-        mempool: Default::default(),
-        services_configs: Default::default(),
-    };
-
-    let mut node = Node::new(blockchain, node_cfg);
-    node.run().unwrap();
-
 
     // Declare Persistent Data
     encoding_struct! {
@@ -278,9 +231,51 @@ fn main() {
         }
     }
 
-
-
-    // Run Service
+    // Initialize Blockchain
+    let db = MemoryDB::new();
     let services: Vec<Box<Service>> = vec![Box::new(CurrencyService)];
+    let blockchain = Blockchain::new(Box::new(db), services);
+
+
+    // Create Keys
+    let (consensus_public_key, consensus_secret_key) = exonum::crypto::gen_keypair();
+    let (service_public_key, service_secret_key) = exonum::crypto::gen_keypair();
+
+
+    // Configure Node
+    let validator_keys = ValidatorKeys {
+        consensus_key: consensus_public_key,
+        service_key: service_public_key,
+    };
+    let genesis = GenesisConfig::new(vec![validator_keys].into_iter());
+
+
+    let api_address = "0.0.0.0:8000".parse().unwrap();
+    let api_cfg = NodeApiConfig {
+        public_api_address: Some(api_address),
+        ..Default::default()
+    };
+
+    let peer_address = "0.0.0.0:2000".parse().unwrap();
+
+    // Complete node configuration
+    let node_cfg = NodeConfig {
+        listen_address: peer_address,
+        peers: vec![],
+        service_public_key,
+        service_secret_key,
+        consensus_public_key,
+        consensus_secret_key,
+        genesis,
+        external_address: None,
+        network: Default::default(),
+        whitelist: Default::default(),
+        api: api_cfg,
+        mempool: Default::default(),
+        services_configs: Default::default(),
+    };
+
+    let mut node = Node::new(blockchain, node_cfg);
+    node.run().unwrap();
 
 }
