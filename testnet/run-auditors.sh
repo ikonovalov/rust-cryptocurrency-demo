c13e2f9ca42e577ebeaeff34c59a8e2d89157fa5
ikonovalov <konovalov84@gmail.com>
2017-07-31 23:13:58 +0300

    Add auditor node

---
+++ testnet/run-auditors.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./cryptocurrency run --node-config testnet1/auditors/0.toml --leveldb testnet1/auditors/db/0 --public-api-address 127.0.0.1:8005 --private-api-address 127.0.0.1:8015
